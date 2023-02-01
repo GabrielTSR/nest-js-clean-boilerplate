755045fb96a0262b9618e9ed0f70b65189a35780
GabrielTSR <gabrielalvesrocha22@gmail.com>
2023-01-31 21:06:47 -0300

    feat: setup files and folder icons, and connection database

--- src/main/config/environment.config.ts
+++ src/main/config/environment.config.ts
@@ -1,19 +1,28 @@
+const oneMinute = 60000;
+const expiresMinutesQuantity = 30;
+const jwtSecretDefaultExpiresInMs = oneMinute * expiresMinutesQuantity;
+
+const API_DEFAULT_PORTS = {
+    build: '3330',
+    development: '3333'
+};
+
+const API_PORTS = {
+    build: process.env.API_PORT_BUILD ?? API_DEFAULT_PORTS.build,
+    development: process.env.API_PORT_DEV ?? API_DEFAULT_PORTS.development
+};
+
+const getApiPort = (): string => {
+    if (typeof process.env.TS_NODE_DEV === 'undefined') return API_PORTS.build;
+    return API_PORTS.development;
+};
+
 export const env = {
-    apiPort:
-        typeof process.env.TS_NODE_DEV === 'undefined'
-            ? process.env.API_PORT_BUILD ?? 3330
-            : process.env.API_PORT_DEV ?? 3333,
-    database: {
-        database: process.env.API_DB_DATABASE ?? '',
-        host: process.env.API_DB_HOST ?? '',
-        password: process.env.API_DB_PASSWORD ?? '',
-        port: Number(process.env.API_DB_PORT) ?? 5432,
-        username: process.env.API_DB_USERNAME ?? ''
-    },
+    apiPort: getApiPort(),
     jwt: {
-        expiresInMs: process.env.JWT_SECRET_EXPIRES_IN ?? 60000 * 60 * 24 * 7,
+        expiresInMs: process.env.JWT_SECRET_EXPIRES_IN ?? jwtSecretDefaultExpiresInMs,
         secret:
-            process.env.JWT_SECRET ?? 'eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZ' // 1 week
+            process.env.JWT_SECRET ?? 'eyJSb2xlIjoiQWRtaW4iLCJJc3N1ZXIiOiJJc3N1ZXIiLCJVc2VybmFtZ'
     },
     staticPaths: {
         images: {
